b35bb3aeffeca883685f3eefb0366333f37252d5
gustavouesso <gustavouesso.gu@gmail.com>
2020-09-07 19:45:43 -0300

    add fields validations

--- AppAC1/app/src/main/java/com/example/appac1/MainActivity.kt
+++ AppAC1/app/src/main/java/com/example/appac1/MainActivity.kt
@@ -14,30 +14,46 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ratingCalc(componente: View) {
+        result.text = ""
         val userName = user_name.text.toString()
-        val userYards = user_yards.text.toString().toDouble()
-        val userTDs = user_td.text.toString().toDouble()
-        val userComp = user_comp.text.toString().toDouble()
-        val userInt = user_int.text.toString().toDouble()
-        val userAtt = user_att.text.toString().toDouble()
+        val userYards = user_yards.text.toString()
+        val userTDs = user_td.text.toString()
+        val userComp = user_comp.text.toString()
+        val userInt = user_int.text.toString()
+        val userAtt = user_att.text.toString()
 
-        val calcResult = ((8.4*userYards)+(330*userTDs)+(100*userComp)-(200*userInt))/userAtt
+        if (userName.isNullOrBlank() || userYards.isNullOrBlank() || userTDs.isNullOrBlank() ||
+            userComp.isNullOrBlank() || userInt.isNullOrBlank() || userAtt.isNullOrBlank()) {
+            Toast.makeText(this, "Campo obrigatório vazio", Toast.LENGTH_SHORT).show()
+        }else {
 
-        if (calcResult > 500){
-            result.text = "Olá ${userName}, seu rating foi ${calcResult}, você é uma lenda"
-            result.setTextColor(Color.BLUE)
-        }else if (calcResult > 0){
-            result.text = "Olá ${userName}, seu rating foi ${calcResult}, você foi muito bem"
-            result.setTextColor(Color.GREEN)
-        }else if (calcResult < -200){
-            result.text = "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar muito"
-            result.setTextColor(Color.RED)
-        }else if (calcResult < 0){
-            result.text = "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar agora"
-            result.setTextColor(Color.YELLOW)
-        }else{
-            result.text = "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar"
-            result.setTextColor(Color.GRAY)
+            if (userComp.toDouble() > userAtt.toDouble()){
+                Toast.makeText(this, "Não é possível ter mais passes completos do que tentados", Toast.LENGTH_SHORT).show()
+            }else {
+                val calcResult =
+                    ((8.4 * userYards.toDouble()) + (330 * userTDs.toDouble()) + (100 * userComp.toDouble()) - (200 * userInt.toDouble())) / userAtt.toDouble()
+
+                if (calcResult > 500) {
+                    result.text = "Olá ${userName}, seu rating foi ${calcResult}, você é uma lenda"
+                    result.setTextColor(Color.BLUE)
+                } else if (calcResult > 0) {
+                    result.text =
+                        "Olá ${userName}, seu rating foi ${calcResult}, você foi muito bem"
+                    result.setTextColor(Color.GREEN)
+                } else if (calcResult < -200) {
+                    result.text =
+                        "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar muito"
+                    result.setTextColor(Color.RED)
+                } else if (calcResult < 0) {
+                    result.text =
+                        "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar agora"
+                    result.setTextColor(Color.YELLOW)
+                } else {
+                    result.text =
+                        "Olá ${userName}, seu rating foi ${calcResult}, você precisa treinar"
+                    result.setTextColor(Color.GRAY)
+                }
+            }
         }
     }
 }
